e686c8082f884e5a67b290fcc91d993455a8fc4f
Joanna <jlapa.transport@gmail.com>
2024-01-11 09:41:32 +0100

    logic for showing panel on click added

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,18 @@
 import styles from "./App.module.css";
+import { useState } from "react";
+import { Button } from "./components/Button/Button";
 import { Panel } from "./components/Panel/Panel";
 
 function App() {
+    const [isPanelShown, setIsPanelShown] = useState(false)
+
+    const handlePanelShow = () => {
+        setIsPanelShown(prevIsPanelShown => !prevIsPanelShown)
+    }
     return (
         <main className={styles.main}>
-            <Panel />
+        <Button onClick={handlePanelShow}>{isPanelShown ? 'Schowaj Panel' : 'Pokaz Panel'}</Button>
+          {isPanelShown && <Panel />}  
         </main>
     );
 }
